baac9d64c27a104dd561c13a2d9f01a553c4aec1
Olubiyitaiwo <olubiyipaul01@gmail.com>
2026-01-16 14:08:30 +0100

    Initial commit: project setup with Spring Boot configdemo

---
+++ src/main/java/org/ecommerce/configdemo/BuildInfoController.java
@@ -0,0 +1,23 @@
+package org.ecommerce.configdemo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BuildInfoController {
+
+    @Value("${build.id}")
+    private String buildId;
+
+    @Value("${build.version}")
+    private String buildVersion;
+
+    @Value("${build.name}")
+    private String buildName;
+
+    @GetMapping("/build-info")
+    public String getBuildInfo(){
+        return "build ID: " + buildId + ", Version: " + buildVersion + ", Name: " + buildName;
+    }
+}
