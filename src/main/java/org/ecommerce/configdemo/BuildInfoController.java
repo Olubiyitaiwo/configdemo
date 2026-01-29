327383c395daa01f93e024a92f46b2c252d214f3
Olubiyitaiwo <olubiyipaul01@gmail.com>
2026-01-29 14:57:16 +0100

    update my controller to take multiple config settings rather than single settings

--- src/main/java/org/ecommerce/configdemo/BuildInfoController.java
+++ src/main/java/org/ecommerce/configdemo/BuildInfoController.java
@@ -1,25 +1,29 @@
 package org.ecommerce.configdemo;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class BuildInfoController {
 
-    @Value("${ID:default}")
-    private String buildId;
+//    @Value("${ID:default}")
+//    private String buildId;
+//
+//    @Value("${VERSION:default}")
+//    private String buildVersion;
+//
+//    @Value("${NAME:default}")
+//    private String buildName;
 
-    @Value("${VERSION:default}")
-    private String buildVersion;
-
-    @Value("${NAME:default}")
-    private String buildName;
+    private BuildInfo buildInfo;
 
     @GetMapping("/build-info")
     public String getBuildInfo() {
-        return "Build ID: " + buildId +
-                ", Version: " + buildVersion +
-                ", Name: " + buildName;
+        return "Build ID: " + buildInfo.getId() +
+                ", Version: " + buildInfo.getVersion() +
+                ", Name: " + buildInfo.getName();
     }
 }
